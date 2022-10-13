6eaec318fa256bdb22e41e752c17e56737d48ec4
aliyan-ansari <aliyanansari321@gmail.com>
2022-10-14 01:21:06 +0500

    mongo db create course

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB...'))
     .catch((err) => console.error('Could not connect to MondoDB error is: ', err));
+
+    // First Make Schema 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPubished: Boolean,
+});
+
+//  compile that in a model
+const Course = mongoose.model('Course', courseSchema);
+async function createCourse() {
+
+    const course = new Course({
+        name: 'React Course',
+        author: 'Ansari',
+        tags: ['react', 'frontend'],
+        isPubished: true,
+    });   
+    const result = await course.save();
+    console.log('Result of Create: ', result);
+}
+
+createCourse();
